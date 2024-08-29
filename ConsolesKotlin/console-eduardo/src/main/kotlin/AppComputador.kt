9fdd847a745130b69c46ae16e0d133ab1640f8b7
EduardoMiyasaki <eduardomiyasaki22@gmail.com>
2024-08-29 20:25:04 -0300

    Adicionando a classe Computador e o file AppComputador

--- ConsolesKotlin/console-eduardo/src/main/kotlin/AppComputador.kt
+++ ConsolesKotlin/console-eduardo/src/main/kotlin/AppComputador.kt
@@ -1,4 +1,56 @@
-fun main(){
+import java.util.Scanner
 
+fun main() {
 
+    val snTexto = Scanner(System.`in`);
+    val snNumero = Scanner(System.`in`);
+
+    val computador1 = Computador();
+
+    computador1.setMarca("Dell");
+    computador1.setModelo("Inspiron 15");
+    computador1.setSistema("Windows");
+    computador1.setProcessador("Intel core i5");
+    computador1.setNucleosCpus(4);
+    computador1.setArmazenamento(16);
+    computador1.setDisco(512);
+    computador1.setMouse(true);
+    computador1.setTeclado(true);
+    computador1.setLigado(true);
+    computador1.atualizarSistema("Linux");
+
+    println(computador1.detalhes())
+    println("----------------------------------")
+
+    val computador2 = Computador();
+
+    print("Informe a marca do seu computador: ");
+    computador2.setMarca(snTexto.nextLine());
+
+    print("Informe o modelo do seu computador: ");
+    computador2.setModelo(snTexto.nextLine());
+
+    print("Informe o sistema operacional: ");
+    computador2.setSistema(snTexto.nextLine());
+
+    print("Informe o nome do processador: ");
+    computador2.setProcessador(snTexto.nextLine());
+
+    print("Informe a quantidade de núcleos da sua CPU: ");
+    computador2.setNucleosCpus(snNumero.nextInt());
+
+    print("Informe a quantidade de armazenamento: ");
+    computador2.setArmazenamento(snNumero.nextInt());
+
+    print("Informe a quantidade de armazenamento do disco: ");
+    computador2.setDisco(snNumero.nextInt());
+
+    println("----------------------------------")
+    println(computador2.detalhes());
+
+    print("Informe a quantidade de armazenamento utilizado: ");
+    println("A Quantidade de armazenamento disponível é ${computador2.calcularArmazenamento(snNumero.nextDouble())} GB");
+
+    computador2.ligar();
+    println(computador2.detalhes());
 }
--- ConsolesKotlin/console-eduardo/src/main/kotlin/Computador.kt
+++ ConsolesKotlin/console-eduardo/src/main/kotlin/Computador.kt
@@ -1,8 +1,143 @@
-class Computador(var teclado: Boolean = false, var mouse: Boolean = false) {
+class Computador(private var teclado: Boolean = false, private var mouse: Boolean = false) {
 
-    var qtdCpus: Int = 0;
-    var qtdArmazenamento: Int = 0;
-    var qtdDisco: Int = 0;
+    private var marca: String = "";
+    private var modelo: String = "";
+    private var sistemaOperacional: String = "";
 
+    private var nomeProcessador: String = "";
+    private var qtdNucleosCpus: Int = 0;
+    private var qtdArmazenamento: Int = 0;
+    private var qtdDisco: Int = 0;
+    private var ligado: Boolean = false;
 
+    // Metódos
+    public fun detalhes(): String {
+
+        return """
+            Marca do computador: ${getMarca()}
+            Modelo do computador: ${getModelo()}
+            Sistema operacional: ${getSistema()}
+            Nome do processador: ${getProcessador()}
+            Quantidade de Núcleos da CPU: ${getCpus()}
+            Quantidade de Armazenamento: ${getArmazenamento()}
+            Quantidade de armazenamento no Disco: ${getDisco()}
+            Teclado está ligado? ${getTeclado()}
+            Mouse está ligado? ${getMouse()}
+            O computador está ligado? ${getLigado()}
+        """.trimIndent()
+    }
+
+    public fun atualizarSistema(sistema: String){
+        this.setSistema(sistema)
+    }
+
+    public fun calcularArmazenamento(usado: Double): Double{
+        // poderia dar o return getDisco() - usado para otimizar
+        val armazenamentoDisponivel = getDisco() - usado;
+        return armazenamentoDisponivel;
+    }
+
+    public fun ligar(){
+        this.ligado = true
+    }
+
+    public fun desligar(){
+        this.ligado = false
+    }
+
+    // Metódos especiais
+    public fun getMarca(): String {
+        return this.marca;
+    }
+
+    public fun setMarca(marca: String) {
+        this.marca = marca;
+    }
+
+    public fun getModelo(): String {
+        return this.modelo
+    }
+
+    public fun setModelo(modelo: String) {
+        this.modelo = modelo;
+    }
+
+    public fun getSistema(): String {
+        return this.sistemaOperacional
+    }
+
+    public fun setSistema(sistema: String) {
+        this.sistemaOperacional = sistema;
+    }
+
+    public fun getProcessador(): String {
+        return this.nomeProcessador;
+    }
+
+    public fun setProcessador(processador: String) {
+        this.nomeProcessador = processador;
+    }
+
+    public fun getCpus(): Int {
+        return this.qtdNucleosCpus
+    }
+
+    public fun setNucleosCpus(cpus: Int) {
+        this.qtdNucleosCpus = cpus;
+    }
+
+    public fun getArmazenamento(): Int {
+        return this.qtdArmazenamento
+    }
+
+    public fun setArmazenamento(armazenamento: Int) {
+        this.qtdArmazenamento = armazenamento;
+    }
+
+    public fun getDisco(): Int {
+        return this.qtdDisco
+    }
+
+    public fun setDisco(disco: Int) {
+        this.qtdDisco = disco;
+    }
+
+    public fun getTeclado(): String {
+        var ligado: String = "Desligado";
+
+        if (teclado) {
+            ligado = "Ligado";
+        }
+        return ligado
+    }
+
+    public fun setTeclado(teclado: Boolean) {
+        this.teclado = teclado
+    }
+
+    public fun getMouse(): String {
+        var ligado: String = "Desligado";
+
+        if (mouse) {
+            ligado = "Ligado";
+        }
+        return ligado
+    }
+
+    public fun setMouse(mouse: Boolean) {
+        this.mouse = mouse;
+    }
+
+    public fun getLigado(): String{
+        var ligad0: String = "Desligado";
+
+        if (ligado) {
+            ligad0 = "Ligado";
+        }
+        return ligad0
+    }
+
+    public fun setLigado(ligado : Boolean){
+        this.ligado = ligado;
+    }
 }
