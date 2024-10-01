99ea381ebdf2293ff9e0128428d47b14f0d7985c
TeixeiraGabbriel <gabriel.teixeira@sptech.school>
2024-09-30 22:40:28 -0300

    Index com quase tudo pronto

--- web-data-viz/public/script/mobile.js
+++ web-data-viz/public/script/mobile.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
     })
 })
 
-window.addEventListener('scroll', function() {
-    let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 0)
-})
+// window.addEventListener('scroll', function() {
+//     let header = document.querySelector('#header')
+//     header.classList.toggle('rolagem', window.scrollY > 0)
+// })
+
