19e3d412c04310a708ab5614ffaf9a4c3e941d8c
TeixeiraGabbriel <gabriel.teixeira@sptech.school>
2024-10-03 15:18:23 -0300

    index com inovação finalizado

--- web-data-viz/public/script/mobile.js
+++ web-data-viz/public/script/mobile.js
@@ -10,3 +10,42 @@ $(document).ready(function(){
 //     header.classList.toggle('rolagem', window.scrollY > 0)
 // })
 
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
+let darkmodeMobile = localStorage.getItem('darkmode')
+const themeSwitchMobile = document.getElementById('mobile-theme-switch')
+
+const enableDarkmodeMobile = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmodeMobile = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmodeMobile === "active") enableDarkmodeMobile()
+
+themeSwitchMobile.addEventListener("click", () => {
+    darkmodeMobile = localStorage.getItem('darkmode')
+    darkmodeMobile !== "active" ? enableDarkmodeMobile() : disableDarkmodeMobile()
+})
